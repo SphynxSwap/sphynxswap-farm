6a9702e81ca4faeaf691ceba658cd08a03856ea7
CryptSans <cryptsans@protonmail.com>
2021-09-24 02:35:36 +0100

    Fix: STC-06 | Unlocked Compiler Version

--- contracts/SphynxToken.sol
+++ contracts/SphynxToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.2;
+pragma solidity 0.6.12;
 
 import '@sphynxswap/sphynx-swap-lib/contracts/access/Manageable.sol';
 import '@sphynxswap/sphynx-swap-lib/contracts/token/BEP20/BEP20.sol';
